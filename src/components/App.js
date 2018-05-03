7bf70c79a9c92f03d64bc0e2d28053b081c757c9
손종국 <conquerex@gmail.com>
2018-05-03 19:37:37 +0900

    Add Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Counter from './Counter';
 
 class App extends Component {
     render() {
         return (
             <div>
-                App
+                <Counter/>
             </div>
         );
     }
