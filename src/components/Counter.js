aae1b9ee37f035d0ef69d36691a15287b63e5cdb
unknown <conquerex@gmail.com>
2018-05-05 02:05:53 +0900

    Randomize Color

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -16,21 +16,42 @@ const defaultProps = {
 
 class Counter extends Component { 
     constructor(props) { 
-        super(props)
+        super(props);
+        this.setRandomColor = this.setRandomColor.bind(this);
+    }
+
+    setRandomColor() {
+        // RGB 세팅
+        const color = [
+            Math.floor((Math.random() * 55) + 200),
+            Math.floor((Math.random() * 55) + 200),
+            Math.floor((Math.random() * 55) + 200)
+        ];
+
+        this.props.handleSetColor(color);
     }
 
     render() {
+
+        const color = this.props.color;
+        const style = {
+            background: `rgb(${color[0]},${color[1]},${color[2]})`
+        };
+
         return(
-            <div>
+            <div style={style}>
                 <Value number={this.props.number}/>
-                <Control/>
+                <Control
+                    onPlus={this.props.handleIncrement}
+                    onSubtract={this.props.handleDecrement}
+                    onRandomizeColor={this.setRandomColor} />
             </div>
         );
     }
 }
 
 Counter.propTypes = propTypes;
-Counter.defaultProps = defaultProps
+Counter.defaultProps = defaultProps;
 
 const mapStateToProps = (state) => {
     return {
