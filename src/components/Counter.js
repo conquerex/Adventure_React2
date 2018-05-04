483cd38f64926342b448e44b157f48b7ec89c5a4
unknown <conquerex@gmail.com>
2018-05-05 01:37:12 +0900

    mapDispatchToProps, mapStateToProps

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import Value from './Value';
 import Control from './Control';
+import { connect, bindActionCreators } from 'react-redux';
+import * as types from '../actions';
 
 const propTypes = {
 
@@ -28,5 +30,27 @@ class Counter extends Component {
 
 Counter.propTypes = propTypes;
 Counter.defaultProps = defaultProps
+
+const mapStateToProps = (state) => {
+    return {
+        number: state.counter.number,
+        color: state.ui.color
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    /**
+     * acions : ActionCreators 들어있는 객체
+     * 이름은 ActionCreators의 이름 그대로 쓰임 
+     * (ex:ActionTypes.js - increment())
+     */
+    // return bindActionCreators(actions, dispatch);
+
+    return {
+        handleIncrement: () => { dispatch(actions.increment())},
+        handleDecrement: () => { dispatch(actions.decrement())},
+        handleSetColor: (color) => { dispatch(actions.setColor(color))}
+    }
+}
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './components/App';
 
 import { createStore } from 'redux';
 import reducers from './reducers';
-import * as actions from './actions';
+import { Provider } from 'react-redux'
 
 /**
  * Store : dispatch(action)
@@ -15,19 +15,10 @@ import * as actions from './actions';
  *          replaceReducer(nextReducer)
  */
 const store = createStore(reducers);
-console.log(store.getState());
-
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-store.dispatch(actions.increment());
-store.dispatch(actions.increment());
-store.dispatch(actions.decrement());
-store.dispatch(actions.setColor([200,200,200]));
-
-unsubscribe();
-
-store.dispatch(actions.setColor([100,100,100]));
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
