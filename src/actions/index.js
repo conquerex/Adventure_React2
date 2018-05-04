1c3e16444cbef16357be3ce1ca74171dab8b666c
unknown <conquerex@gmail.com>
2018-05-04 22:37:48 +0900

    Add action types

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+// import {INCREMENT, DECREMENT, SET_COLOR} from './ActionTypes';
+import * as types from './ActionTypes';
+
+// 액션 생성자
+export function increment() {
+    return {
+        type: types.INCREMENT
+    };
+}
+
+export function decrement() {
+    return {
+        type: types.DECREMENT
+    };
+}
+
+export function setColor(color) {
+    return {
+        type: types.SET_COLOR,
+        color
+    };
+}
